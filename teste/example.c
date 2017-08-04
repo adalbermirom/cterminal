2b4999cdeae18ac7d358b3a0f89b98310029bb0b
adalbermirom <30734786+adalbermirom@users.noreply.github.com>
2017-08-04 17:50:46 -0300

    Update example.c

--- teste/example.c
+++ teste/example.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 
 #define _NO_READLINE //define no readline if you do not want to use lib readline. (linking is necessary if use it);
-#include "../terminal.h"
+#include "../cterminal/terminal.h"
 
 int main(int argc, char **argv)
 { 
-	t_settitle("Meu teste em term"); //define o titulo
+	t_settitle("My test - cterminal"); //define o titulo
 	t_setbackground(tWhite); //define cor de fundo branca.
 	t_setforeground(tRed); //define cor da letra vermelha.
 	puts("ola mundo");
